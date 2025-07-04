a4e9552a366f3a7a15006751d4d3b21a997ab793
TakenHara <kavlod12345@gmail.com>
2025-07-04 12:13:05 +0300

    feat: update station block logic

--- src/js/app/actionBlock.ts
+++ src/js/app/actionBlock.ts
@@ -3,36 +3,42 @@ import gsap from 'gsap';
 class ActionBlock {
     actionItems;
     contentItems;
+    infoBubbles;
     
     constructor(actionItems: NodeListOf<Element>) {
         this.actionItems = actionItems;
         this.contentItems = document.querySelectorAll('[data-action-text]')
+        this.infoBubbles = document.querySelectorAll('[data-action-bubble]')
         
         this.init()
     }
     
     init() {
         this.actionItems.forEach((el, idx) => {
-            el.addEventListener('mouseenter', (evt) => {
-                const element = this.contentItems.item(idx) as HTMLElement;
-                
-                gsap.set(element, {display: 'flex'})
-                
-                gsap.fromTo(element,
-                    {autoAlpha: 0},
-                    {autoAlpha: 1, duration: 0.2}
-                );
-            })
-            
-            el.addEventListener('mouseleave', (evt) => {
-                const element = this.contentItems.item(idx) as HTMLElement;
-                gsap.to(element, {
-                    autoAlpha: 0,
-                    duration: 0.1,
-                    onComplete: () => element.style.display = 'none'
-                });
+            el.addEventListener('click', () => {
+                this.infoBubbles.forEach(temp => temp.classList.remove('active'))
+                const infoBubble = el.querySelector('[data-action-bubble]');
+                if (!infoBubble) return;
+                this.updateInfoBubble(infoBubble);
             })
         })
+        
+        this.infoBubbles.forEach((el) => {
+            const closeBtn = el.querySelector('.action-map__close')
+            closeBtn.addEventListener('click', (evt) => {
+                evt.preventDefault()
+                evt.stopPropagation()
+                el.classList.remove('active')
+            })
+            
+            el.addEventListener("mouseleave", () => {
+                el.classList.remove('active')
+            })
+        })
+    }
+    
+    updateInfoBubble = (element: Element) => {
+        element.classList.toggle('active');
     }
 }
 
